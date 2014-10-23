b6d6911b4f0c306d5afdbeb342c4cfeac14113a9
Jordoser <Jmservic@ualberta.ca>
2014-10-22 18:12:21 -0600

    submission for Lab6

--- lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/IntentReaderActivityTest.java
+++ lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/IntentReaderActivityTest.java
@@ -4,6 +4,7 @@ package ca.ualberta.cs.lonelytwitter.test;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ViewAsserts;
+import android.view.View;
 import android.widget.TextView;
 import ca.ualberta.cs.lonelytwitter.IntentReaderActivity;
 
@@ -16,5 +17,81 @@ public class IntentReaderActivityTest extends
     }
 
 //TODO: Add your code here:
+    
+   public void testSendText(){
+	   Intent intent= new Intent();
+	   intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Testing send text");
+	   
+	   setActivityIntent(intent);
+	   
+	   IntentReaderActivity ira = getActivity();
+	   assertEquals(ira.getText(),"Testing send text");
+	   
+   }
        
+   public void testDisplayText(){
+	   Intent intent = new Intent();
+	   intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Testing Display");
+	   
+	   setActivityIntent(intent);
+	   
+	   IntentReaderActivity ira = getActivity();
+	   TextView tv = (TextView)ira.findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+	   
+	   assertEquals(tv.getText(),"Testing Display");
+   }
+   
+   public void testDoubleText(){
+	   Intent intent = new Intent();
+	   intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Testing Double");
+	   intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY, IntentReaderActivity.DOUBLE);
+	   
+	   setActivityIntent(intent);
+	   
+	   IntentReaderActivity ira = getActivity();
+	   TextView tv = (TextView)ira.findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+	   
+	   assertEquals(tv.getText(),"Testing DoubleTesting Double");
+   }
+   
+   public void testReverseText(){
+	   Intent intent = new Intent();
+	   intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Test");
+	   intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY, IntentReaderActivity.REVERSE);
+	   
+	   setActivityIntent(intent);
+	   
+	   IntentReaderActivity ira = getActivity();
+	   TextView tv = (TextView)ira.findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+	   
+	   assertEquals(tv.getText(),"tseT");
+   }
+   
+   public void testDefault(){
+	   Intent intent = new Intent();
+	   
+	   setActivityIntent(intent);
+	   
+	   IntentReaderActivity ira = getActivity();
+	   TextView tv = (TextView)ira.findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+	   
+	   assertEquals(tv.getText(),"default text");
+	   
+	   
+   }
+   
+   public void testViewOnScreen(){
+	   Intent intent = new Intent();
+	   
+	   setActivityIntent(intent);
+	   
+	   IntentReaderActivity ira = getActivity();
+	   View test = ira.getWindow().getDecorView();
+	   TextView tv = (TextView)ira.findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+	   ViewAsserts.assertOnScreen(test, tv);
+
+	   
+	   
+   }
+   
 }
